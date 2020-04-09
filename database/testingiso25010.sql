8dca285e9fba20d5ba889f169504e5286bdfaec1
ulfaza <ulfatuzzahroh2@gmail.com>
2020-04-09 14:44:35 +0700

    dbv1

--- database/testingiso25010.sql
+++ database/testingiso25010.sql
@@ -4,132 +4,132 @@
 /*==============================================================*/
 
 
-drop table if exists aplikasi;
+DROP TABLE IF EXISTS aplikasi;
 
-drop table if exists hasil_kuesioner;
+DROP TABLE IF EXISTS hasil_kuesioner;
 
-drop table if exists karakteristik;
+DROP TABLE IF EXISTS karakteristik;
 
-drop table if exists penilaian_karakteristik;
+DROP TABLE IF EXISTS penilaian_karakteristik;
 
-drop table if exists penilaian_sub_karakteristik;
+DROP TABLE IF EXISTS penilaian_sub_karakteristik;
 
-drop table if exists responden;
+DROP TABLE IF EXISTS responden;
 
-drop table if exists sub_karakteristik;
+DROP TABLE IF EXISTS sub_karakteristik;
 
-drop table if exists user;
+DROP TABLE IF EXISTS users;
 
 /*==============================================================*/
 /* table: user                                                  */
 /*==============================================================*/
-create table user
+CREATE TABLE users
 (
-   username             varchar(32) not null,
-   nama                 varchar(128),
-   role                 varchar(32),
-   instansi             varchar(128),
-   password             varchar(32),
-   primary key (username)
+   username             VARCHAR(32) NOT NULL,
+   nama                 VARCHAR(128),
+   role                 VARCHAR(32),
+   instansi             VARCHAR(128),
+   pwd             VARCHAR(32),
+   PRIMARY KEY (username)
 );
 
 /*==============================================================*/
 /* table: aplikasi                                              */
 /*==============================================================*/
-create table aplikasi
+CREATE TABLE aplikasi
 (
-   id_aplikasi          int not null auto_increment,
-   username             varchar(32),
-   nama_aplikasi        varchar(256),
-   nama_instansi        varchar(256),
-   total                float(6),
-   primary key (id_aplikasi)
+   id_aplikasi          INT NOT NULL AUTO_INCREMENT,
+   username             VARCHAR(32),
+   nama_aplikasi        VARCHAR(256),
+   nama_instansi        VARCHAR(256),
+   total                FLOAT(6),
+   PRIMARY KEY (id_aplikasi)
 );
 
 /*==============================================================*/
 /* table: karakteristik                                         */
 /*==============================================================*/
-create table karakteristik
+CREATE TABLE karakteristik
 (
-   id_karakteristik     int not null auto_increment,
-   nama_karakteristik   varchar(64),
-   primary key (id_karakteristik)
+   id_karakteristik     INT NOT NULL AUTO_INCREMENT,
+   nama_karakteristik   VARCHAR(64),
+   PRIMARY KEY (id_karakteristik)
 );
 
 /*==============================================================*/
 /* table: sub_karakteristik                                     */
 /*==============================================================*/
-create table sub_karakteristik
+CREATE TABLE sub_karakteristik
 (
-   id_sub_karakteristik int not null auto_increment,
-   id_karakteristik     char(3) not null,
-   nama_sub_karakteristik varchar(64),
-   primary key (id_sub_karakteristik)
+   id_sub_karakteristik INT NOT NULL AUTO_INCREMENT,
+   id_karakteristik     INT NOT NULL,
+   nama_sub_karakteristik VARCHAR(64),
+   PRIMARY KEY (id_sub_karakteristik)
 );
 
 /*==============================================================*/
 /* table: responden                                             */
 /*==============================================================*/
-create table responden
+CREATE TABLE responden
 (
-   id_responden         int not null auto_increment,
-   nama_responden       varchar(256),
-   primary key (id_responden)
+   id_responden         INT NOT NULL AUTO_INCREMENT,
+   nama_responden       VARCHAR(256),
+   PRIMARY KEY (id_responden)
 );
 
 /*==============================================================*/
 /* table: penilaian_karakteristik                               */
 /*==============================================================*/
-create table penilaian_karakteristik
+CREATE TABLE penilaian_karakteristik
 (
-   id_penilaian_karakteristik int not null auto_increment,
-   id_aplikasi          int,
-   id_karakteristik     char(3),
-   bobot_karakteristik  float(6),
-   nilai_karakteristik  float(6),
-   primary key (id_penilaian_karakteristik)
+   id_penilaian_karakteristik INT NOT NULL AUTO_INCREMENT,
+   id_aplikasi          INT,
+   id_karakteristik     INT,
+   bobot_karakteristik  FLOAT(6),
+   nilai_karakteristik  FLOAT(6),
+   PRIMARY KEY (id_penilaian_karakteristik)
 );
 
 /*==============================================================*/
 /* table: penilaian_sub_karakteristik                           */
 /*==============================================================*/
-create table penilaian_sub_karakteristik
+CREATE TABLE penilaian_sub_karakteristik
 (
-   id_penilaian_sub_karakteristik int not null auto_increment,
-   id_penilaian_karakteristik int not null,
-   bobot_relatif        float(6),
-   nilai_sub            float(6),
-   primary key (id_penilaian_sub_karakteristik)
+   id_penilaian_sub_karakteristik INT NOT NULL AUTO_INCREMENT,
+   id_penilaian_karakteristik INT NOT NULL,
+   bobot_relatif        FLOAT(6),
+   nilai_sub            FLOAT(6),
+   PRIMARY KEY (id_penilaian_sub_karakteristik)
 );
 
 /*==============================================================*/
 /* table: hasil_kuesioner                                       */
 /*==============================================================*/
-create table hasil_kuesioner
+CREATE TABLE hasil_kuesioner
 (
-   id_penilaian_sub_karakteristik int not null,
-   id_responden         int,
-   nilai                int
+   id_penilaian_sub_karakteristik INT NOT NULL,
+   id_responden         INT,
+   nilai                INT
 );
 
-alter table aplikasi add constraint fk_relationship_7 foreign key (username)
-      references user (username) on delete cascade on update cascade;
+ALTER TABLE aplikasi ADD CONSTRAINT fk_relationship_7 FOREIGN KEY (username)
+      REFERENCES users (username) ON DELETE CASCADE ON UPDATE CASCADE;
 
-alter table hasil_kuesioner add constraint fk_mendapat_nilai foreign key (id_penilaian_sub_karakteristik)
-      references penilaian_sub_karakteristik (id_penilaian_sub_karakteristik) on delete cascade on update cascade;
+ALTER TABLE sub_karakteristik ADD CONSTRAINT fk_relationship_8 FOREIGN KEY (id_karakteristik)
+      REFERENCES karakteristik (id_karakteristik) ON DELETE CASCADE ON UPDATE CASCADE;
 
-alter table hasil_kuesioner add constraint fk_relationship_9 foreign key (id_responden)
-      references responden (id_responden) on delete cascade on update cascade;
+ALTER TABLE penilaian_karakteristik ADD CONSTRAINT fk_memilih FOREIGN KEY (id_aplikasi)
+      REFERENCES aplikasi (id_aplikasi) ON DELETE CASCADE ON UPDATE CASCADE;
 
-alter table penilaian_karakteristik add constraint fk_memilih foreign key (id_aplikasi)
-      references aplikasi (id_aplikasi) on delete cascade on update cascade;
+ALTER TABLE penilaian_karakteristik ADD CONSTRAINT fk_relationship_6 FOREIGN KEY (id_karakteristik)
+      REFERENCES karakteristik (id_karakteristik) ON DELETE CASCADE ON UPDATE CASCADE;
 
-alter table penilaian_karakteristik add constraint fk_relationship_6 foreign key (id_karakteristik)
-      references karakteristik (id_karakteristik) on delete cascade on update cascade;
+ALTER TABLE penilaian_sub_karakteristik ADD CONSTRAINT fk_memiliki FOREIGN KEY (id_penilaian_karakteristik)
+      REFERENCES penilaian_karakteristik (id_penilaian_karakteristik) ON DELETE CASCADE ON UPDATE CASCADE;
 
-alter table penilaian_sub_karakteristik add constraint fk_memiliki foreign key (id_penilaian_karakteristik)
-      references penilaian_karakteristik (id_penilaian_karakteristik) on delete cascade on update cascade;
+ALTER TABLE hasil_kuesioner ADD CONSTRAINT fk_mendapat_nilai FOREIGN KEY (id_penilaian_sub_karakteristik)
+      REFERENCES penilaian_sub_karakteristik (id_penilaian_sub_karakteristik) ON DELETE CASCADE ON UPDATE CASCADE;
 
-alter table sub_karakteristik add constraint fk_relationship_8 foreign key (id_karakteristik)
-      references karakteristik (id_karakteristik) on delete cascade on update cascade;
+ALTER TABLE hasil_kuesioner ADD CONSTRAINT fk_relationship_9 FOREIGN KEY (id_responden)
+      REFERENCES responden (id_responden) ON DELETE CASCADE ON UPDATE CASCADE;
 
