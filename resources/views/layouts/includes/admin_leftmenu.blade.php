83ede7395e1f415676661bb5f6d89fbfd4d2180d
itsnadh <itsnadzakia@gmail.com>
2020-04-11 10:57:27 +0700

    Nambah file admin_leftmenu

---
+++ resources/views/layouts/includes/admin_leftmenu.blade.php
@@ -0,0 +1,29 @@
+<div class="container-fluid mimin-wrapper">
+  
+  <!-- start:Left Menu -->
+    <div id="left-menu">
+      <div class="sub-left-menu scroll">
+        <ul class="nav nav-list">
+            <li><div class="left-bg"></div></li>
+            <li class="time">
+              <h1 class="animated fadeInLeft">21:00</h1>
+              <p class="animated fadeInRight">Sat,October 1st 2029</p>
+            </li>
+            <li class="active ripple">
+              <a class="tree-toggle nav-header"><span class="fa-user fa"></span> Edit Profil 
+               </a>
+            </li>
+            <li class="ripple">
+              <a class="tree-toggle nav-header">
+                <span class="fa-balance-scale fa"></span> Bobot Relatif
+              </a>
+            </li>
+            <li class="ripple">
+              <a class="tree-toggle nav-header">
+                <span class="fa-book fa"></span> Kelola Admin
+               </a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  <!-- end: Left Menu -->
