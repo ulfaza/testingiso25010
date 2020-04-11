b316bf67ae0da957cc920ec8e922364972715d08
itsnadh <itsnadzakia@gmail.com>
2020-04-11 14:38:16 +0700

    tambah file view home_admin.blade

---
+++ resources/views/admin/home_admin.blade.php
@@ -0,0 +1,35 @@
+@include('layouts.includes.admin_header')
+@section('menu_dashboard')
+
+                <div class="col-md-12" style="padding:100px; padding-top:20%">
+ 
+                    <div class="col-md-4">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center"> 
+                                <a href="/admin/profil"><h1>Profil</h1></a>
+                                <hr/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center">
+                                <a href="/admin/bobotrelatif"><h1>Bobot Relatif</h1></a>
+                                <hr/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="panel box-v1">
+                            <div class="panel-body text-center" style="">
+                            <a href="/admin/kelolaadmin"><h1>Kelola Admin</h1></a>
+                                <hr/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
